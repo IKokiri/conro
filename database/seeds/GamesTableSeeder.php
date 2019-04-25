17497f78bf4c155535f58e2f7addc088bfefc3dc
Luiz Mendes <developer.luizmendes@gmail.com>
2019-04-25 09:55:24 -0300

    adiconado seeder da tabela games

---
+++ database/seeds/GamesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GamesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('games')->insert([
+            'id' => 1,
+            'open' => 1,
+            'price' => 0.5,
+            'quantity' => 1
+        ]);
+    }
+}
